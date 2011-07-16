089a0f1a300d2aed6f367ab527885b054d714a68
robertsdionne <robertsdionne@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2011-07-16 03:18:34 +0000

    Implementing an HTTP gateway to replace use of Jetty's ProxyServlet.Transparent.
    git-svn-id: https://js-test-driver.googlecode.com/svn/trunk@982 205c1ca0-44cf-11de-84a6-839b6ed6c72e

---
+++ JsTestDriver/src/com/google/jstestdriver/server/proxy/GatewayServlet.java
@@ -0,0 +1,100 @@
+// Copyright 2011 Google, Inc. All Rights Reserved.
+package com.google.jstestdriver.server.proxy;
+
+import com.google.inject.Inject;
+import com.google.jstestdriver.requesthandlers.HttpMethod;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethodBase;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * An HTTP gateway that forwards requests to another server and feeds the responses back to the
+ * client.
+ * TODO(rdionne): Write unit tests.
+ * TODO(rdionne): Implement HOST header and 302 redirect host substitution.
+ * @author rdionne@google.com (Robert Dionne)
+ */
+public class GatewayServlet extends JstdProxyServlet {
+
+  private final HttpClient client;
+  private String proxyTo;
+
+  @Inject
+  public GatewayServlet(final HttpClient client) {
+    this.client = client;
+  }
+
+  @Override
+  public void init(final ServletConfig servletConfig) throws ServletException {
+    proxyTo = servletConfig.getInitParameter("ProxyTo");
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    throw new UnsupportedOperationException("#getSerlvetConfig() not supported.");
+  }
+
+  @Override
+  public void service(final ServletRequest req, final ServletResponse res)
+        throws ServletException, IOException {
+    final HttpServletRequest request = (HttpServletRequest) req;
+    final HttpMethodBase method = getMethod(request);
+    addRequestHeaders(method, request);
+    method.setQueryString(request.getQueryString());
+    final int statusCode = client.executeMethod(method);
+    final HttpServletResponse response = (HttpServletResponse) res;
+    response.setStatus(statusCode);
+    addResponseHeaders(method, response);
+    Streams.copy(method.getResponseBodyAsStream(), response.getOutputStream());
+    method.releaseConnection();
+  }
+
+  private void addRequestHeaders(final HttpMethodBase method, final HttpServletRequest request) {
+    final Enumeration headers = request.getHeaderNames();
+    while (headers.hasMoreElements()) {
+      final String name = (String) headers.nextElement();
+      final Enumeration values = request.getHeaders(name);
+      while (values.hasMoreElements()) {
+        final String value = (String) values.nextElement();
+        method.addRequestHeader(name, value);
+      }
+    }
+  }
+
+  private void addResponseHeaders(final HttpMethodBase method, final HttpServletResponse response) {
+    for (final Header header : method.getResponseHeaders()) {
+      response.addHeader(header.getName(), header.getValue());
+    }
+  }
+
+  private HttpMethodBase getMethod(final HttpServletRequest request) throws IOException {
+    final HttpMethod method = HttpMethod.valueOf(request.getMethod());
+    switch (method) {
+      case POST:
+      case PUT:
+        return new GatewayEntityMethod(
+            method.name(), proxyTo + request.getRequestURI(), request.getInputStream());
+      default:
+        return new GatewayMethod(method.name(), proxyTo + request.getRequestURI());
+    }
+  }
+
+  @Override
+  public String getServletInfo() {
+    throw new UnsupportedOperationException("#getServletInfo() not supported.");
+  }
+
+  @Override
+  public void destroy() {
+    throw new UnsupportedOperationException("#destroy() not supported.");
+  }
+}
--- JsTestDriver/src/com/google/jstestdriver/server/proxy/ProxyBehavior.java
+++ JsTestDriver/src/com/google/jstestdriver/server/proxy/ProxyBehavior.java
@@ -2,17 +2,25 @@
 package com.google.jstestdriver.server.proxy;
 
 /**
+ * Enum describing proxy/gateway behavior.
+ * TODO(rdionne): Eliminate all references to {@link ProxyBehavior}.
+ * @deprecated Just use {@link GatewayServlet}.
  * @author rdionne@google.com (Robert Dionne)
  */
 public enum ProxyBehavior {
 
   /**
-   *
+   * Forward the HOST header.
    */
   FORWARD,
 
   /**
-   *
+   * Do The Right Thing(TM).
+   */
+  GATEWAY,
+
+  /**
+   * Spoof the HOST header.
    */
   SPOOF
 }
