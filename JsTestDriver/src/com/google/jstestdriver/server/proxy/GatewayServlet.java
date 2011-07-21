b67d560c03dd9b9257778feb5d6e8d8aa8f38ae9
robertsdionne <robertsdionne@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2011-07-21 00:50:01 +0000

    Intercept 301,302,303,307 redirects and spoof the location header.
    git-svn-id: https://js-test-driver.googlecode.com/svn/trunk@990 205c1ca0-44cf-11de-84a6-839b6ed6c72e

--- JsTestDriver/src/com/google/jstestdriver/server/proxy/GatewayServlet.java
+++ JsTestDriver/src/com/google/jstestdriver/server/proxy/GatewayServlet.java
@@ -7,6 +7,7 @@ import com.google.jstestdriver.requesthandlers.HttpMethod;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethodBase;
+import org.mortbay.jetty.Response;
 
 import java.io.IOException;
 import java.net.URI;
@@ -30,6 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 public class GatewayServlet extends JstdProxyServlet {
 
   private static final String HOST = "Host";
+  private static final String LOCATION = "Location";
   private static final String DESTROY_NOT_SUPPORTED = "#destroy() not supported.";
   private static final String GET_SERLVET_CONFIG_NOT_SUPPORTED =
       "#getSerlvetConfig() not supported.";
@@ -69,35 +71,12 @@ public class GatewayServlet extends JstdProxyServlet {
     final HttpServletResponse response = (HttpServletResponse) res;
     response.setStatus(statusCode);
     addResponseHeaders(method, response);
+    if (isRedirect(statusCode)) {
+      spoofLocationHeader(request, (Response) response);
+    }
     // TODO(rdionne): Substitute the JsTD server address for the proxyTo address in any redirects.
     Streams.copy(method.getResponseBodyAsStream(), response.getOutputStream());
     method.releaseConnection();
-  }
-
-  private void addRequestHeaders(final HttpMethodBase method, final HttpServletRequest request) {
-    final Enumeration headers = request.getHeaderNames();
-    while (headers.hasMoreElements()) {
-      final String name = (String) headers.nextElement();
-      final Enumeration values = request.getHeaders(name);
-      while (values.hasMoreElements()) {
-        final String value = (String) values.nextElement();
-        method.addRequestHeader(name, value);
-      }
-    }
-  }
-
-  private void spoofHostHeader(HttpMethodBase method) {
-    try {
-      method.setRequestHeader(HOST, new URI(proxyTo).getAuthority());
-    } catch (URISyntaxException badUriSyntax) {
-      throw new RuntimeException(badUriSyntax);
-    }
-  }
-
-  private void addResponseHeaders(final HttpMethodBase method, final HttpServletResponse response) {
-    for (final Header header : method.getResponseHeaders()) {
-      response.addHeader(header.getName(), header.getValue());
-    }
   }
 
   private HttpMethodBase getMethod(final HttpServletRequest request) throws IOException {
@@ -116,6 +95,76 @@ public class GatewayServlet extends JstdProxyServlet {
             method.name(), url, request.getInputStream());
       default:
         return new GatewayMethod(method.name(), url);
+    }
+  }
+
+  private void addRequestHeaders(final HttpMethodBase method, final HttpServletRequest request) {
+    final Enumeration headers = request.getHeaderNames();
+    while (headers.hasMoreElements()) {
+      final String name = (String) headers.nextElement();
+      final Enumeration values = request.getHeaders(name);
+      while (values.hasMoreElements()) {
+        final String value = (String) values.nextElement();
+        method.addRequestHeader(name, value);
+      }
+    }
+  }
+
+  private void spoofHostHeader(HttpMethodBase method) {
+    method.setRequestHeader(HOST, parseUri(proxyTo).getAuthority());
+  }
+
+  private URI parseUri(String uri) {
+    try {
+      return new URI(uri);
+    } catch (URISyntaxException badUriSyntax) {
+      throw new RuntimeException(badUriSyntax);
+    }
+  }
+
+  private void addResponseHeaders(final HttpMethodBase method, final HttpServletResponse response) {
+    for (final Header header : method.getResponseHeaders()) {
+      response.addHeader(header.getName(), header.getValue());
+    }
+  }
+
+  private boolean isRedirect(int statusCode) {
+    switch (statusCode) {
+      case HttpServletResponse.SC_MOVED_PERMANENTLY:
+      case HttpServletResponse.SC_FOUND:
+      case HttpServletResponse.SC_SEE_OTHER:
+      case HttpServletResponse.SC_TEMPORARY_REDIRECT:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  void spoofLocationHeader(HttpServletRequest request, Response response) {
+    URI location = parseUri(response.getHeader(LOCATION));
+    String host = request.getServerName();
+    int port = request.getServerPort();
+    URI proxyTo = parseUri(this.proxyTo);
+    if (!location.getHost().equals(proxyTo.getHost())) {
+      return;
+    }
+    if (location.getPort() == proxyTo.getPort()
+        || location.getPort() == -1 && (proxyTo.getPort() == 80 || proxyTo.getPort() == 443)) {
+      response.setHeader(LOCATION,
+          buildLocationHeader(location, host, port));
+    }
+  }
+
+  private String buildLocationHeader(URI location, String host, int port) {
+    try {
+      return new URI(
+          location.getScheme(),
+          String.format("%s:%s", host, port),
+          location.getPath(),
+          location.getQuery(),
+          location.getFragment()).toString();
+    } catch (URISyntaxException badUriSyntax) {
+      throw new RuntimeException(badUriSyntax);
     }
   }
 
