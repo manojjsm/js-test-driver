4a0075c50fb1cb9fff8ba396ad29ef74081632d2
corbinrsmith@gmail.com <corbinrsmith@gmail.com@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2011-07-28 17:12:12 +0000

    Revert "Implementing an HTTP gateway to replace use of Jetty's ProxyServlet.Transparent."
    This reverts commit 089a0f1a300d2aed6f367ab527885b054d714a68.
    
    git-svn-id: https://js-test-driver.googlecode.com/svn/trunk@995 205c1ca0-44cf-11de-84a6-839b6ed6c72e

---
+++ JsTestDriver/src/com/google/jstestdriver/server/proxy/HostForwardingJstdProxyServlet.java
@@ -0,0 +1,12 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+package com.google.jstestdriver.server.proxy;
+
+import org.mortbay.servlet.ProxyServlet;
+
+/**
+ * A {@link Transparent} that we could reconfigure to
+ * intercept various headers.
+ * @author rdionne@google.com (Robert Dionne)
+ */
+public class HostForwardingJstdProxyServlet extends JstdProxyServlet {
+}
