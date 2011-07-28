8dd837d3a508e96e662ed6e7be0f7bb8ece37477
corbinrsmith@gmail.com <corbinrsmith@gmail.com@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2011-07-28 17:12:53 +0000

    Revert "Revert "Adding unit tests for the new gateway classes.""
    This reverts commit 216ffccd138608fe7206b9d45154b528384e95b8.
    
    git-svn-id: https://js-test-driver.googlecode.com/svn/trunk@999 205c1ca0-44cf-11de-84a6-839b6ed6c72e

---
+++ JsTestDriver/src-test/com/google/jstestdriver/server/proxy/GatewayEntityMethodTest.java
@@ -0,0 +1,25 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+package com.google.jstestdriver.server.proxy;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.httpclient.methods.EntityEnclosingMethod;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author rdionne@google.com (Robert Dionne)
+ */
+public class GatewayEntityMethodTest extends TestCase {
+
+  public void testGetNameGetUriAndGetRequestEntity() throws Exception {
+    ByteArrayInputStream in = new ByteArrayInputStream("ASDF".getBytes());
+    EntityEnclosingMethod method = new GatewayEntityMethod("POST", "http://www.google.com/search", in);
+    assertEquals("POST", method.getName());
+    assertEquals("http://www.google.com/search", method.getURI().toString());
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    method.getRequestEntity().writeRequest(out);
+    assertEquals("ASDF", out.toString());
+  }
+}
---
+++ JsTestDriver/src-test/com/google/jstestdriver/server/proxy/GatewayMethodTest.java
@@ -0,0 +1,18 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+package com.google.jstestdriver.server.proxy;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.httpclient.HttpMethod;
+
+/**
+ * @author rdionne@google.com (Robert Dionne)
+ */
+public class GatewayMethodTest extends TestCase {
+
+  public void testGetNameAndGetUri() throws Exception {
+    HttpMethod method = new GatewayMethod("GET", "http://www.google.com/search");
+    assertEquals("GET", method.getName());
+    assertEquals("http://www.google.com/search", method.getURI().toString());
+  }
+}
